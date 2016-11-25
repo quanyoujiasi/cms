651b7741209e5da8f3845b729f3808d8852d202a
maxwelldu <dcj3sjt@126.com>
2016-11-25 10:07:42 +0800

    add .gitignore and update article controller

--- Application/Common/Model/ArticleModel.class.php
+++ Application/Common/Model/ArticleModel.class.php
@@ -1,9 +1,20 @@
 <?php
 namespace Common\Model;
 use Think\Model;
+
+/**
+ * 文章模型
+ * Class ArticleModel
+ * @package Common\Model
+ */
 class ArticleModel extends Model {
-    public function addArticles($data) {
-      $this->create($data);
+    /**
+     * 添加文章
+     * @param array $data 文章的数据
+     * @return mixed
+     */
+    public function addArticle($data) {
+        $this->create($data);
         return $this->add();
     }
 }
