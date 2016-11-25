8de85214c83fe948be4acec3acf32706a17f76d4
maxwelldu <dcj3sjt@126.com>
2016-11-25 11:34:15 +0800

    delete .idea and runtime

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -8,10 +8,6 @@ use Think\Controller;
  * @package Admin\Controller
  */
 class ArticleController extends Controller {
-    public function index() {
-
-        $this->display();
-    }
     /**
      * 显示添加文章页面
      */
@@ -28,22 +24,26 @@ class ArticleController extends Controller {
      * 添加文章动作
      */
     public function addAction() {
-        $data['title'] = I('post.title');
-        $data['cid'] = I('post.cate');
+        $data['title']      = I('post.title');
+        $data['cid']        = I('post.cate');
         $data['descrption'] = I('post.description');
-        $data['author'] = I('post.author');
-        $data['addtime'] = I('post.add_time');
-        $data['status'] = I('post.status');
-        $data['a_order'] = I('post.ord');
-        $data['content'] = htmlspecialchars(I('post.content'));
+        $data['author']     = I('post.author');
+        $data['addtime']    = I('post.add_time');
+        $data['status']     = I('post.status');
+        $data['a_order']    = I('post.ord');
+        $data['content']    = htmlspecialchars(I('post.content'));
         $res = D('Article')->addArticle($data);//把文章内容插入数据库
         if ($res) {
             return $this->redirect('article/index');
         }
         return $this->error('添加文章失败');
     }
+
+    /**
+     * 显示文章列表
+     */
     public function ArticleList(){
-    	$data=D('article')->getArticle();
+    	$data = D('article')->getArticle();
         $this->assign('data',$data);
         $this->display('ArticleList');
     }
